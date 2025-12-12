7232f65444867b532f7c705cc402fff97c046468
Mitch Jans <jansmitch@gmail.com>
2025-12-12 13:37:09 +0100

    init 2025 days 11 and 12

---
+++ years/2025/11/index.ts
@@ -0,0 +1,35 @@
+import * as R from 'ramda';
+
+const year = 2025;
+const day = 11;
+
+// problem: https://adventofcode.com/2025/day/11
+
+const aoc2025_day11_part1 = async (input: string, ...params: any[]) => {
+  const startTime = process.hrtime();	
+  
+	const result = "Not implemented";
+  
+	const endTime = process.hrtime(startTime);
+  console.log(`Y${year} D${day} P1 Time taken: ${endTime[0]}s ${endTime[1] / 1000000}ms`);
+  
+	return result;
+}
+
+const aoc2025_day11_part2 = async (input: string, ...params: any[]) => {
+  const startTime = process.hrtime();
+  
+	const result = "Not implemented";
+  
+	const endTime = process.hrtime(startTime);
+  console.log(`Y${year} D${day} P2 Time taken: ${endTime[0]}s ${endTime[1] / 1000000}ms`);
+  
+	return result;
+}
+
+export default {
+	year,
+	day,
+  part1: aoc2025_day11_part1,
+  part2: aoc2025_day11_part2,
+};
